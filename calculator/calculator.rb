f965b76f33ca8082356698bb2073150587e715a6
raj kushwah <kushwahraaj7@gmail.com>
2024-05-08 00:34:28 +0530

    feat: Returns 1 for given argument is '1'

--- calculator/calculator.rb
+++ calculator/calculator.rb
@@ -1,5 +1,10 @@
 class Calculator 
-	def add(a)
-		return 0 if a.empty?
+	def add(input)
+		if input.empty?
+			return 0 
+		else 
+			input = input.gsub(/\s*\W+\s*/, ",").split(',').map(&:to_i)
+			return input.inject(&:+)
+		end
 	end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -4,5 +4,12 @@ describe Calculator do
     result = calculator.add('')
     expect(result).to eq(0)
   end
+
+  it 'returns 1 for given argument is 1' do 
+    calculator = Calculator.new
+    result = calculator.add('1')
+    expect(result).to eq(1)
+  end
+
 end
 
